d51e74898adc95cb1bc05cf0504683434a1ff97c
SamanthBommishetti <160122609+SamanthBommishetti@users.noreply.github.com>
2024-11-30 15:10:43 +0530

    Add files via upload

---
+++ hangman_stages.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+stages = [
+    """
+    -----
+    |   |
+    |   O
+    |  /|\\
+    |  / \\
+    ---
+    """,  # Final stage: hanged
+    """
+    -----
+    |   |
+    |   O
+    |  /|\\
+    |  /
+    ---
+    """,  # 1 life left
+    """
+    -----
+    |   |
+    |   O
+    |  /|\\
+    |
+    ---
+    """,  # 2 lives left
+    # Add more stages here as needed
+]
+
+
+# In[3]:
+
+
+print(stages)
+
+
+# In[ ]:
+
+
+
+
